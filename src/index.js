d739089173a94ba5b9ff322930815fb3217d628d
HunterTigerX <HunterTigerX@gmail.com>
2021-08-22 01:46:09 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+        if (n > 0) {
+            return Number(n.toString().split("").reverse().join(""));
+         } else {
+             let moduleN = (n - n - n);
+            return Number(moduleN.toString().split("").reverse().join(""));
+    }
 }
